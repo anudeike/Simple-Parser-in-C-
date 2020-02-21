12f2e8881057f7cdcab3de98bdce7e96aabd7fa3
anudeike <ikechukwuanude@gmail.com>
2020-02-21 02:52:54 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * CALCULATOR PROGRAM
+ */
+
+namespace SimpleTokenizer
+{
+    // main program class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
