9f0cba66b3231246c2eab8b6b93640095f7a38c0
anudeike <ikechukwuanude@gmail.com>
2020-03-25 01:06:07 -0500

    added a process list function

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Mime;
 using System.Runtime.CompilerServices;
@@ -11,7 +12,6 @@ using System.Runtime.CompilerServices;
  * this handles simple operations such as 3 + 6
  *
  * Grammar ->
- * GOAL ::= ASSIGNMENT EOF
  * ASSIGNMENT ::= LHS '=' RHS
  * LHS = IDENTIFIER
  * RHS = IDENTIFIER | NUMBER
@@ -20,9 +20,32 @@ using System.Runtime.CompilerServices;
 
 namespace SimpleTokenizer
 {
+    // token class
+    class Token
+    {
+        // this class merely represents a token
+        
+        public string Value;
+        public string Type;
+        
+        public Token(string type, string value)
+        {
+            // make them both come in a strings
+            this.Type = type;
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            // give a string representation of the class
+            return $"TOKEN: ({Type}, {Value})";
+        }
+    }
     class Tokenizer
     {
         public List<Token> tokens;
+        public List<Token> processed_tokens;
+        
         public int pos;
         
         public Tokenizer(string raw_text)
@@ -31,64 +54,6 @@ namespace SimpleTokenizer
             this.pos = 0;
         }
         
-        // generate the token list
-        private List<Token> GenerateTokenList(string text)
-        {
-            // create a temp list to hold the tokens
-            List<Token> t = new List<Token>();
-
-            // for each token in the Regex Split
-            foreach (var symbol in Regex.Split(text,@"\s+"))
-            {
-                string type = "";
-                
-                // if it matches a digit
-                if (Regex.IsMatch(symbol, @"\d+"))
-                {
-                    type = "INTEGER";
-                    // add to the token list
-                    t.Add(new Token(type, symbol));
-                    
-                    // go thru the cases to check what type of variable is coming
-                    Console.WriteLine(symbol + " " + type);
-                    
-                    continue;
-                }
-                
-                // if it matches a word
-                if (Regex.IsMatch(symbol, @"\w+"))
-                {
-                    type = "IDENTIFIER";
-                    
-                    // add to the token list
-                    t.Add(new Token(type, symbol));
-                    
-                    // go thru the cases to check what type of variable is coming
-                    Console.WriteLine(symbol + " " + type);
-                    continue;
-                }
-
-                // if it matches an equal sign
-                if (symbol.Equals("="))
-                {
-                    type = "EQUALS_SIGN";
-                    
-                    // add to the token list
-                    t.Add(new Token(type, symbol));
-                    
-                    // go thru the cases to check what type of variable is coming
-                    Console.WriteLine(symbol + " " + type);
-                    continue;
-                }
-                
-                
-            }
-            
-            // end an eof token to the end -> will use the new line character for the time being
-            t.Add(new Token("EOF", "\n")); 
-            return t;
-        }
-
         private List<Token> GenerateTokenUsingSplit(string text)
         {
             // take in the string and use the match function to match function to create a token
@@ -103,6 +68,9 @@ namespace SimpleTokenizer
                 t.Add(Match(word));
             }
             
+            // add the EOF token
+            t.Add(new Token("EOF", "EOF"));
+
             return t;
         }
 
@@ -164,214 +132,63 @@ namespace SimpleTokenizer
 
         public Token Peek()
         {
-            // returns the next token in list w/o changing the pos
-            if (this.pos == this.tokens.Count)
+            if (this.pos == this.processed_tokens.Count)
             {
-                // stub will continue later.
+                
             }
+            
+            // // returns the next token in list w/o changing the pos
+            // if (this.pos == this.tokens.Count)
+            // {
+            //     // stub will continue later.
+            // }
             return null;
         }
         
     }
-    
-    // token class
-    class Token
-    {
-        // this class merely represents a token
-        
-        public string Value;
-        public string Type;
-        
-        public Token(string type, string value)
-        {
-            // make them both come in a strings
-            this.Type = type;
-            this.Value = value;
-        }
-
-        public override string ToString()
-        {
-            // give a string representation of the class
-            return $"TOKEN: ({Type}, {Value})";
-        }
-    }
 
     class Parser
-    {
-        public List<Token> tokens;
+    { 
+        /*
+        * Grammar ->
+        * ASSIGNMENT ::= LHS '=' RHS
+        * LHS = IDENTIFIER
+        * RHS = IDENTIFIER | NUMBER
+        *
+        */
         
-        public Parser(List<Token> tks)
+        private Tokenizer _tk;
+        private Token _nextToken;
+        public Parser(Tokenizer tk)
         {
-            this.tokens = tks;
+            this._tk = tk;
+            
+            //do while to lex()
+            do
+            {
+                tk.GetToken();
+            } while (_nextToken.Type != "EOF");
         }
 
-        public bool ParseTokens()
+        private void ParseAssignment()
         {
-            if (tokens == null)
-            {
-                // get mad
-                Console.WriteLine("The tokens list is empty...");
-                return false;
-            }
             
-            
-            foreach (var tk in tokens)
-            {
-                
-                Parse_Goal(tk);
-            }
-            return false;
-        }
-        public void ThrowError(string type, Token t)
-        {
-            // crude but will be improved on later
-            if (type.Equals("syntax"))
-            {
-                Console.WriteLine($"SYNTAX ERROR with token: {t.Value}");
-            }
-            
-            if (type.Equals("unknown"))
-            {
-                Console.WriteLine($"UNKNOWN TOKEN: {t.Value}");
-            }
         }
         
-        // list of parsing functions
-        public bool Parse_Goal(Token tk)
-        {
-            if (tk.Type.Equals("EOF"))
-            {
-                Console.WriteLine("reached the end");
-                return false;
-            }
-            
-            if (Parse_Assignment(tk))
-            {
-                Console.WriteLine("Parsed correctly");
-                return true;
-            }
-            
-            // if it is neither of the above then it is not in the language
-            ThrowError("unknown", tk);
-            
-            return false;
-        }
-        
-        public bool Parse_Assignment(Token tk)
-        {
-            // not sure why the logic inverts here -_-
-            
-            // according to the grammar given
-            if (Parse_LHS(tk))
-            { 
-                return true;
-            }
-
-            if (Parse_Equals(tk))
-            {
-                return true;
-            }
-
-            if (Parse_RHS(tk))
-            {
-                return true;
-            }
-
-            
-            
-            ThrowError("syntax", tk);
-            return false;
-        }
-        
-        public bool Parse_LHS(Token tk)
-        {
-            // according to the grammar given
-            if (Parse_Identifer(tk))
-            {
-                Console.WriteLine("\nFound LHS: ");
-                return true;
-            }
-            return false;
-        }
-        
-        public bool Parse_RHS(Token tk)
-        {
-            // according to the grammar given
-            if (Parse_Identifer(tk))
-            {
-                Console.WriteLine("\nFound RHS: ");
-                return true;
-            }
-
-            if (Parse_Integer(tk))
-            {
-                Console.WriteLine("\nFound RHS");
-                return true;
-            }
-            return false;
-        }
-        
-        // TERMINALS
-        public bool Parse_Equals(Token tk)
-        {
-            if (tk.Type.Equals("EQUALS_SIGN"))
-            {
-                Console.WriteLine("\nFound Equal Sign: ");
-                return true;
-            }
-            
-            return false;
-        }
-        
-        public bool Parse_Identifer(Token tk)
-        {
-            if (tk.Type.Equals("IDENTIFIER"))
-            {
-                Console.WriteLine("\nFound Identifier: ");
-                return true;
-            }
-            
-            return false;
-        }
-        
-        public bool Parse_Integer(Token tk)
-        {
-            // test input to see if it is an integer
-            // this is a terminal, so you only need to test if it is what it is
-            if (tk.Type.Equals("INTEGER"))
-            {
-                Console.WriteLine("\nFound Integer: ");
-                return true;
-            }
-            
-            return false;
-        }
+        // 
         
     }
+    
+    
     // main program class
     class Program
     {
         static void Main(string[] args)
         {
-            string example = "a = 9; b = 9;";
+            string example = "a = 9";
             
             // tokenize
             Tokenizer tk = new Tokenizer(example);
-            
-            /*// source code that we will test
-            string code = "a = 9;";
-
-            // push thru the tokenizer
-            Tokenizer tk = new Tokenizer(code);
-            
-            // push the tokens into the parser
-            Parser pr = new Parser(tk.tokens);
-
-            pr.ParseTokens();*/
-            // foreach (var t in pr.tokens)
-            // {
-            //     Console.WriteLine(t.ToString());
-            // }
         }
     }
 }
